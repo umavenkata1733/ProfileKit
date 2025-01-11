34fe71797395d1bc93c5ce5c8932e30a741ce9b5
umavenkata1733 <umavenkata1733@gmail.com>
2025-01-11 14:04:33 -0500

    Initial Commit

---
+++ ProfileKit/ProfileKit.h
@@ -0,0 +1,18 @@
+//
+//  ProfileKit.h
+//  ProfileKit
+//
+//  Created by Anand on 1/11/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProfileKit.
+FOUNDATION_EXPORT double ProfileKitVersionNumber;
+
+//! Project version string for ProfileKit.
+FOUNDATION_EXPORT const unsigned char ProfileKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProfileKit/PublicHeader.h>
+
+
